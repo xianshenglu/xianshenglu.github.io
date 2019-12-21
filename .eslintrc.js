1a5d4f2921f0e0bd6e5f0f1d5427399b413fbac1
xianshenglu <xianshenglu@foxmail.com>
2019-12-21 23:01:11 +0800

    chore: update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: ['@xianshenglu/eslint-config-vue'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 2019
   },
   overrides: [
     {
