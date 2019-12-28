5d71d28319ca70d85347ed54f065f2c1281073e9
xianshenglu <xianshenglu@qq.com>
2019-12-28 22:15:03 +0800

    style: format only

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,40 +1,58 @@
 <template>
- <section class="page page-project" >
-   <div class="page-project-detail project-name">
-     <h3 class="project-name-text">{{project.name}}</h3>
+  <section class="page page-project">
+    <div class="page-project-detail project-name">
+      <h3 class="project-name-text">{{ project.name }}</h3>
     </div>
-   <ul class="page-project-detail project-data">
-     <li class="project-data-list" v-for="(item,index) in project.data" :key="index">
-       <img :src="item.img" class="list-img">
-       <div class="list-description">
-         <h5 class="list-description-detail description-title">{{item.description.title}}</h5>
-         <ul class="list-description-detail description-highlight">
-           <li class="description-highlight-list"  v-for="(highlight,hlIndex) in item.description.highlights" :key="'highlight'+hlIndex">
-             <span class="highlight-list-icon"></span>
-             <span class="highlight-list-text">{{highlight}}</span>
-           </li>
-         </ul>
-       </div>
-       <div class="list-link">
-         <a :href="item.source.url" class="list-link-detail" target="_blank">{{item.source.name}}</a>
-         <a :href="item.preview.url" class="list-link-detail" target="_blank">{{item.preview.name}}</a>
-       </div>
-     </li>
-   </ul>
-   <div class="page-project-detail project-hint">
-     <span class="project-hint-name">{{project.more.name}}</span>
-     <a :href="project.more.url" class="project-hint-link" target="_blank">
-      <svg :class="'icon link-img '+project.more.icon" aria-hidden="true">
-          <use :xlink:href="'#'+project.more.icon"></use>
-      </svg>
-     </a>
-   </div>
- </section>
+    <ul class="page-project-detail project-data">
+      <li
+        v-for="(item, index) in project.data"
+        :key="index"
+        class="project-data-list"
+      >
+        <img :src="item.img" class="list-img" />
+        <div class="list-description">
+          <h5 class="list-description-detail description-title">
+            {{ item.description.title }}
+          </h5>
+          <ul class="list-description-detail description-highlight">
+            <li
+              v-for="(highlight, hlIndex) in item.description.highlights"
+              :key="'highlight' + hlIndex"
+              class="description-highlight-list"
+            >
+              <span class="highlight-list-icon"></span>
+              <span class="highlight-list-text">{{ highlight }}</span>
+            </li>
+          </ul>
+        </div>
+        <div class="list-link">
+          <a :href="item.source.url" class="list-link-detail" target="_blank">{{
+            item.source.name
+          }}</a>
+          <a
+            :href="item.preview.url"
+            class="list-link-detail"
+            target="_blank"
+            >{{ item.preview.name }}</a
+          >
+        </div>
+      </li>
+    </ul>
+    <div class="page-project-detail project-hint">
+      <span class="project-hint-name">{{ project.more.name }}</span>
+      <a :href="project.more.url" class="project-hint-link" target="_blank">
+        <svg :class="'icon link-img ' + project.more.icon" aria-hidden="true">
+          <use :xlink:href="'#' + project.more.icon"></use>
+        </svg>
+      </a>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Project',
+  // eslint-disable-next-line vue/require-prop-types
   props: ['project']
 }
 </script>
@@ -42,13 +60,14 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .page-project {
-  display: flex;
   align-items: center;
+  display: flex;
   flex-flow: column nowrap;
-  justify-content: center;
   height: auto;
+  justify-content: center;
   min-height: 100vh;
 }
+/* stylelint-disable-next-line  */
 _:-ms-lang(x),
 .page-project {
   /*IE10 and Edge will ignore flex property with height:auto*/
@@ -58,8 +77,8 @@ _:-ms-lang(x),
   flex: 1 1 auto;
 }
 .page-project .project-name {
-  display: flex;
   align-items: center;
+  display: flex;
 }
 @media screen and (max-width: 760px) {
   .page-project .project-name {
@@ -71,8 +90,8 @@ _:-ms-lang(x),
   font-weight: 900;
 }
 .page-project .project-data {
-  display: flex;
   align-content: center;
+  display: flex;
   justify-content: center;
 
   width: 100%;
@@ -85,14 +104,14 @@ _:-ms-lang(x),
   }
 }
 .project-data .project-data-list {
-  display: flex;
   align-items: center;
-  flex-flow: column nowrap;
 
   box-sizing: border-box;
-  width: 250px;
+  display: flex;
+  flex-flow: column nowrap;
   margin: 10px 20px;
   padding: 10px 20px;
+  width: 250px;
 }
 @media screen and (max-width: 760px) {
   .project-data .project-data-list {
@@ -100,11 +119,11 @@ _:-ms-lang(x),
   }
 }
 .project-data-list .list-img {
-  width: 200px;
   height: 200px;
   margin: 0 auto;
 
   transition: 1s transform ease;
+  width: 200px;
 }
 .project-data-list .list-img:hover {
   transform: scale(1.1);
@@ -113,12 +132,12 @@ _:-ms-lang(x),
   display: flex;
   flex: 1 1 auto;
   flex-flow: column nowrap;
-  width: 95%;
-  max-width: 250px;
-  max-height: 200px;
-  margin: 20px 20px;
 
   line-height: 1.5;
+  margin: 20px;
+  max-height: 200px;
+  max-width: 250px;
+  width: 95%;
 }
 
 .list-description .description-title {
@@ -138,14 +157,14 @@ _:-ms-lang(x),
   text-align: left;
 }
 .description-highlight-list .highlight-list-icon {
-  margin-right: 5px;
-  display: inline-block;
-  width: 6px;
-  height: 6px;
+  background: rgb(0, 0, 0);
   border-radius: 50%;
-  background: #000;
+  display: inline-block;
+  height: 6px;
+  margin-right: 5px;
   vertical-align: middle;
   vertical-align: 2px;
+  width: 6px;
 }
 .project-data-list .list-link {
   display: flex;
@@ -153,39 +172,37 @@ _:-ms-lang(x),
   width: 100%;
 }
 .list-link .list-link-detail {
+  border: 1px solid rgb(0, 0, 0);
+  border-radius: 20px;
   display: block;
   flex: 1 0 auto;
 
-  margin: 0 10px 0 0;
-
-  border: 1px solid black;
-  border-radius: 20px;
-
   line-height: 30px;
+
+  margin: 0 10px 0 0;
 }
 .list-link .list-link-detail:last-child {
   margin-right: 0;
 }
 .page-project .project-hint {
-  margin-bottom: 20px;
-  display: flex;
   align-items: center;
-}
-.project-hint .project-hint-link:hover {
-  cursor: pointer;
+  display: flex;
+  margin-bottom: 20px;
 }
 .project-hint .project-hint-link {
   display: inline-block;
   vertical-align: top;
 }
+.project-hint .project-hint-link:hover {
+  cursor: pointer;
+}
 .project-hint-link .link-img {
+  border-radius: 50%;
   display: block;
-
-  width: 25px;
   height: 25px;
   margin: 0;
 
-  border-radius: 50%;
+  width: 25px;
 }
 .project-hint .project-hint-name {
   margin: 0 10px;
