ce079072a3b94521123e47c007d14b6bda77f6ae
xianshenglu <xianshenglu@qq.com>
2019-12-29 17:51:08 +0800

    chore: release v2.0.0

---
+++ dist/precache-manifest.1009bf06fcf9a00a3856fd3a3be36f9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7bf99370a17955ec690",
+    "url": "/css/app.caa4fa8b.css"
+  },
+  {
+    "revision": "ce07c7e2674654faa70631502fbc875a",
+    "url": "/img/avator.ce07c7e2.png"
+  },
+  {
+    "revision": "e755cdcee513fda0ee9e6cd730b180f6",
+    "url": "/img/blog.e755cdce.gif"
+  },
+  {
+    "revision": "316ec1aa28a71c38d322430db5b09476",
+    "url": "/img/homepage.316ec1aa.gif"
+  },
+  {
+    "revision": "47644041ece19b032991dc0b79f41d98",
+    "url": "/img/to-do-mvc.47644041.gif"
+  },
+  {
+    "revision": "62dbd32dc9a7cecb62fbd731b4994220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7bf99370a17955ec690",
+    "url": "/js/app.ce9b257e.js"
+  },
+  {
+    "revision": "c15dc3133a002ac37d5c",
+    "url": "/js/chunk-vendors.c395da29.js"
+  },
+  {
+    "revision": "f89f8374651268753b67b93ec3ec0dbe",
+    "url": "/js/iconfont.js"
+  },
+  {
+    "revision": "33cc540bd226fcc605dcbecdaa93e0eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
