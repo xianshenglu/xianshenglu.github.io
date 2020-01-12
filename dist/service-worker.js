0c4d318c945eea2f1f87412ec726fe8914c16cfd
xianshenglu <xianshenglu@qq.com>
2020-01-12 22:05:25 +0800

    chore: release v2.5.0

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,34 +1,12 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts("https://cdn.jsdelivr.net/gh/xianshenglu/xianshenglu.github.io/dist/precache-manifest.78a39b77974e43d48c5b72641c8c5459.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "precache-manifest.ba270215de13eaf12768991ac375ccfd.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "xianshenglu.github.io"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
+self.addEventListener('message', e => {
+  if (e.data && e.data.type === 'SKIP_WAITING') {
+    self.skipWaiting()
   }
-});
+})
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+/* eslint-disable no-undef */
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
