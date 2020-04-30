f19bb2c2e875f834929a960862f296051ea19de0
xianshenglu <xianshenglu@qq.com>
2020-05-01 00:05:49 +0800

    chore: release v2.6.0

---
+++ dist/precache-manifest.5fe7feb97afea93fddc1ac09f3732cfe.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25a0e1151afc78b6a30",
+    "url": "css/Index.e3acc4a6.css"
+  },
+  {
+    "revision": "7d558dd389d3944812ee",
+    "url": "css/Introduction.45c7c913.css"
+  },
+  {
+    "revision": "3cba7cac75409113ca71",
+    "url": "css/Project.8abb0626.css"
+  },
+  {
+    "revision": "71b54eefae2718838bca",
+    "url": "css/Skill.24d5a2c8.css"
+  },
+  {
+    "revision": "0599cd430cda3bec43d5",
+    "url": "css/UnitLang.8999fb0c.css"
+  },
+  {
+    "revision": "bd8a9dacfdc5af7f573d",
+    "url": "css/UnitNav.d23d0dfc.css"
+  },
+  {
+    "revision": "cc87ce7c1369ff624a64",
+    "url": "css/UpgradeButton.c5f866dc.css"
+  },
+  {
+    "revision": "d55aface4c597d4a03e9",
+    "url": "css/app.fb6914dc.css"
+  },
+  {
+    "revision": "279b0d80d66da632b8f9c5ee30665d6e",
+    "url": "img/avator.279b0d80.png"
+  },
+  {
+    "revision": "8fc8975fe09d9b6f31ef8048fe2c5a31",
+    "url": "img/blog.8fc8975f.gif"
+  },
+  {
+    "revision": "03a081e3c07ab2e6faf4d083a495f2c6",
+    "url": "img/homepage.03a081e3.gif"
+  },
+  {
+    "revision": "b223dc087cdc1e85017203275c5d572c",
+    "url": "img/to-do-mvc.b223dc08.gif"
+  },
+  {
+    "revision": "f8120fae09c7de4554ea22c2eafbac4b",
+    "url": "index.html"
+  },
+  {
+    "revision": "c25a0e1151afc78b6a30",
+    "url": "js/Index.ac28435e.js"
+  },
+  {
+    "revision": "7d558dd389d3944812ee",
+    "url": "js/Introduction.8cd0cc48.js"
+  },
+  {
+    "revision": "3cba7cac75409113ca71",
+    "url": "js/Project.855ca5e0.js"
+  },
+  {
+    "revision": "71b54eefae2718838bca",
+    "url": "js/Skill.b102cde4.js"
+  },
+  {
+    "revision": "0599cd430cda3bec43d5",
+    "url": "js/UnitLang.e1b5e93b.js"
+  },
+  {
+    "revision": "bd8a9dacfdc5af7f573d",
+    "url": "js/UnitNav.70dd3eda.js"
+  },
+  {
+    "revision": "cc87ce7c1369ff624a64",
+    "url": "js/UpgradeButton.dcfb5c19.js"
+  },
+  {
+    "revision": "d55aface4c597d4a03e9",
+    "url": "js/app.a8c4f16f.js"
+  },
+  {
+    "revision": "ec251021f1b765425895",
+    "url": "js/chunk-vendors.4f2870d3.js"
+  },
+  {
+    "revision": "f89f8374651268753b67b93ec3ec0dbe",
+    "url": "js/iconfont.js"
+  },
+  {
+    "revision": "a59447c933b85eb68e352509f49abe09",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b69a07cd29fc4fc4e5cba7195125fd7a",
+    "url": "service-worker.js"
+  }
+]);
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://cdn.jsdelivr.net/gh/xianshenglu/xianshenglu.github.io/dist/precache-manifest.78a39b77974e43d48c5b72641c8c5459.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.5fe7feb97afea93fddc1ac09f3732cfe.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 self.addEventListener('message', e => {
   if (e.data && e.data.type === 'SKIP_WAITING') {
@@ -6,7 +6,12 @@ self.addEventListener('message', e => {
   }
 })
 
-self.__precacheManifest = [].concat(self.__precacheManifest || [])
+self.__precacheManifest = [
+  {
+    url: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
+    revision: null
+  }
+].concat(self.__precacheManifest || [])
 /* eslint-disable no-undef */
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
