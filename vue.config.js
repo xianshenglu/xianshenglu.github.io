748b96a16afee90aae8c8e94f9558e7c6228fe90
xianshenglu <xianshenglu@qq.com>
2020-05-07 23:03:56 +0800

    build: extract node_modules lib bundles, and remove hash in lib, app files
    for better performance and give more support for server push

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,36 @@
+const serverPushPath = 'server-push'
 module.exports = {
   lintOnSave: false,
+  css: {
+    extract: {
+      moduleFilename: module => `${serverPushPath}/css/${module.name}.css`
+    }
+  },
   chainWebpack: config => {
-    config.externals({
-      vue: 'Vue'
+    config.output.filename(`${serverPushPath}/js/[name].js`)
+    config.optimization.splitChunks({
+      cacheGroups: {
+        common: {
+          name: 'chunk-common',
+          minChunks: 2,
+          priority: -20,
+          chunks: 'initial',
+          reuseExistingChunk: true
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.resource.match(
+              /\\node_modules\\([^\\]*?)\\/
+            )[1]
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm-${packageName.replace('@', '-')}`
+          },
+          filename: `${serverPushPath}/js/[name].js`,
+          priority: -10,
+          chunks: 'initial'
+        }
+      }
     })
   },
   publicPath: './',
