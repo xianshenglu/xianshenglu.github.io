d41a8f2bd3b9b3fee8c976c86d7346f3ca09d83d
xianshenglu <xianshenglu@qq.com>
2020-05-18 15:58:16 +0800

    chore: remove unused vue precache in cdn

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,11 +4,6 @@ self.addEventListener('message', e => {
   }
 })
 
-self.__precacheManifest = [
-  {
-    url: 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    revision: null
-  }
-].concat(self.__precacheManifest || [])
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
 /* eslint-disable no-undef */
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
